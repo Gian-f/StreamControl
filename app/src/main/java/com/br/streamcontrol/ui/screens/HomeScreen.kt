42027feefa0a7ee2285ec768c318c882e758057c
Gian Felipe <gianfelipe87@gmail.com>
2023-11-12 13:12:08 -0300

    implements home content

--- app/src/main/java/com/br/streamcontrol/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/br/streamcontrol/ui/screens/HomeScreen.kt
@@ -1,6 +1,8 @@
 package com.br.streamcontrol.ui.screens
 
 import android.net.Uri
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -12,12 +14,23 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.outlined.KeyboardArrowDown
+import androidx.compose.material.icons.outlined.KeyboardArrowUp
+import androidx.compose.material.icons.outlined.MoreVert
 import androidx.compose.material.icons.twotone.Add
+import androidx.compose.material3.Badge
+import androidx.compose.material3.BadgedBox
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
@@ -32,17 +45,21 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.AsyncImage
+import com.br.streamcontrol.R
 import com.br.streamcontrol.data.dummy.NavigationItemDummy.navItems
 import com.br.streamcontrol.domain.routes.Router
 import com.br.streamcontrol.domain.routes.Screen
@@ -138,51 +155,271 @@ private fun HomeContent(
     homeViewModel: HomeViewModel,
 ) {
     val imageUri by remember { mutableStateOf(homeViewModel.localUserPhoto.value) }
-    Row(
-        horizontalArrangement = Arrangement.Start,
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(contentPadding)
-    ) {
-        Surface(
-            color = MaterialTheme.colorScheme.surfaceVariant,
-            shape = CircleShape,
-            modifier = Modifier.padding(start = 8.dp)
+    Column {
+        Row(
+            horizontalArrangement = Arrangement.Start,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(contentPadding)
         ) {
-            if (imageUri != Uri.EMPTY) {
-                AsyncImage(
-                    modifier = Modifier.size(50.dp),
-                    contentScale = ContentScale.FillBounds,
-                    model = imageUri,
-                    contentDescription = null,
-                    alignment = Alignment.Center
-                )
-            } else {
-                Box(
-                    modifier = Modifier.size(60.dp),
-                    contentAlignment = Alignment.Center
-                ) {
-                    Icon(
-                        imageVector = Icons.Filled.Person,
-                        modifier = Modifier.size(30.dp),
+            Surface(
+                color = MaterialTheme.colorScheme.surfaceVariant,
+                shape = CircleShape,
+                modifier = Modifier.padding(start = 8.dp)
+            ) {
+                if (imageUri != Uri.EMPTY) {
+                    AsyncImage(
+                        modifier = Modifier.size(50.dp),
+                        contentScale = ContentScale.FillBounds,
+                        model = imageUri,
                         contentDescription = null,
-                        tint = MaterialTheme.colorScheme.onSurfaceVariant
+                        alignment = Alignment.Center
+                    )
+                } else {
+                    Box(
+                        modifier = Modifier.size(60.dp),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Icon(
+                            imageVector = Icons.Filled.Person,
+                            modifier = Modifier.size(30.dp),
+                            contentDescription = null,
+                            tint = MaterialTheme.colorScheme.onSurfaceVariant
+                        )
+                    }
+                }
+            }
+            Column {
+                Text(
+                    text = "Seja bem vindo,",
+                    fontSize = TextUnit(16F, TextUnitType.Sp),
+                    modifier = Modifier.padding(horizontal = 16.dp)
+                )
+                Text(
+                    text = homeViewModel.emailId.collectAsState().value,
+                    fontSize = TextUnit(20F, TextUnitType.Sp),
+                    fontWeight = FontWeight.SemiBold,
+                    modifier = Modifier.padding(horizontal = 16.dp)
+                )
+            }
+        }
+        LazyColumn {
+            item {
+                UsageCard()
+            }
+            item {
+                Spacer(modifier = Modifier.height(20.dp))
+                EntertainmentCard()
+                Spacer(modifier = Modifier.height(20.dp))
+            }
+        }
+    }
+}
+
+@Composable
+private fun UsageCard() {
+    Column {
+        ElevatedCard(
+            elevation = CardDefaults.cardElevation(2.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(150.dp)
+                .padding(horizontal = 16.dp)
+        ) {
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(
+                    text = "Uso",
+                    fontSize = TextUnit(17F, TextUnitType.Sp),
+                    fontWeight = FontWeight.W600,
+                    modifier = Modifier.padding(horizontal = 16.dp)
+                )
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(
+                        imageVector = Icons.Outlined.MoreVert,
+                        contentDescription = "arrowDown"
                     )
                 }
             }
+            Row(
+                horizontalArrangement = Arrangement.Start,
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp)
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.netflix),
+                    modifier = Modifier.height(30.dp),
+                    contentDescription = "netflix"
+                )
+                Text(
+                    text = "Netflix",
+                    fontSize = TextUnit(16F, TextUnitType.Sp),
+                    fontWeight = FontWeight.SemiBold,
+                    modifier = Modifier.padding(horizontal = 16.dp)
+                )
+            }
+            Spacer(modifier = Modifier.height(4.dp))
+            Row {
+                Row(
+                    horizontalArrangement = Arrangement.Start,
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Column {
+                        Text(
+                            text = "12:27 h",
+                            fontSize = TextUnit(20F, TextUnitType.Sp),
+                            fontWeight = FontWeight.Medium,
+                            modifier = Modifier.padding(horizontal = 16.dp)
+                        )
+                        Spacer(modifier = Modifier.height(4.dp))
+                        Text(
+                            text = "Essa semana",
+                            textAlign = TextAlign.End,
+                            fontSize = TextUnit(13F, TextUnitType.Sp),
+                            fontWeight = FontWeight.W500,
+                            modifier = Modifier.padding(horizontal = 16.dp)
+                        )
+                    }
+                }
+            }
         }
-        Column {
-            Text(
-                text = "Seja bem vindo,",
-                fontSize = TextUnit(16F, TextUnitType.Sp),
-                modifier = Modifier.padding(horizontal = 16.dp)
-            )
-            Text(
-                text = homeViewModel.emailId.collectAsState().value,
-                fontSize = TextUnit(20F, TextUnitType.Sp),
-                fontWeight = FontWeight.SemiBold,
-                modifier = Modifier.padding(horizontal = 16.dp)
-            )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun EntertainmentCard() {
+    var cardHeight by remember { mutableStateOf(120.dp) }
+
+    ElevatedCard(
+        elevation = CardDefaults.cardElevation(2.dp),
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(cardHeight)
+            .padding(horizontal = 16.dp)
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Text(
+                    text = "Entretenimento",
+                    fontSize = TextUnit(17F, TextUnitType.Sp),
+                    fontWeight = FontWeight.W600,
+                    modifier = Modifier.padding(horizontal = 16.dp)
+                )
+                BadgedBox(badge = {
+                    Badge {
+                        Text("3")
+                    }
+                }) {}
+            }
+            IconButton(onClick = { cardHeight = if (cardHeight == 120.dp) 250.dp else 120.dp }) {
+                Icon(
+                    imageVector = if (cardHeight == 120.dp) Icons.Outlined.KeyboardArrowDown else Icons.Outlined.KeyboardArrowUp,
+                    contentDescription = "arrow"
+                )
+            }
+        }
+        Text(
+            text = "R$ 250,00",
+            textAlign = TextAlign.Center,
+            fontSize = TextUnit(22F, TextUnitType.Sp),
+            fontWeight = FontWeight.W500,
+            modifier = Modifier
+                .padding(horizontal = 16.dp)
+                .fillMaxWidth()
+        )
+        Text(
+            text = "Total Gasto",
+            textAlign = TextAlign.Center,
+            fontSize = TextUnit(13F, TextUnitType.Sp),
+            fontWeight = FontWeight.W500,
+            modifier = Modifier
+                .padding(horizontal = 16.dp)
+                .fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(40.dp))
+        Divider(modifier = Modifier.padding(horizontal = 16.dp))
+        Spacer(modifier = Modifier.height(20.dp))
+        LazyColumn {
+            item {
+                Row(
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp)
+                ) {
+                    Row(
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically,
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.netflix),
+                            modifier = Modifier.height(30.dp).width(25.dp),
+                            contentDescription = "netflix"
+                        )
+                        Text(
+                            text = "Netflix",
+                            fontSize = TextUnit(16F, TextUnitType.Sp),
+                            fontWeight = FontWeight.SemiBold,
+                            modifier = Modifier.padding(horizontal = 16.dp)
+                        )
+                    }
+
+                    Text(
+                        text = "R$ 100,00",
+                        fontSize = TextUnit(14F, TextUnitType.Sp),
+                        fontWeight = FontWeight.W500,
+                        modifier = Modifier.padding(horizontal = 16.dp)
+                    )
+                }
+            }
+            item {
+                Row(
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp)
+                ) {
+                    Row(
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically,
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.primevideo),
+                            modifier = Modifier.height(30.dp).width(25.dp),
+                            contentDescription = "amazon"
+                        )
+                        Text(
+                            text = "Prime Video",
+                            fontSize = TextUnit(16F, TextUnitType.Sp),
+                            fontWeight = FontWeight.SemiBold,
+                            modifier = Modifier.padding(horizontal = 16.dp)
+                        )
+                    }
+
+                    Text(
+                        text = "R$ 150,00",
+                        fontSize = TextUnit(14F, TextUnitType.Sp),
+                        fontWeight = FontWeight.W500,
+                        modifier = Modifier.padding(horizontal = 16.dp)
+                    )
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/br/streamcontrol/ui/screens/navigation/ChartScreen.kt
+++ app/src/main/java/com/br/streamcontrol/ui/screens/navigation/ChartScreen.kt
@@ -1,51 +1,99 @@
 package com.br.streamcontrol.ui.screens.navigation
 
+import android.widget.Toast
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.br.streamcontrol.domain.viewmodel.HomeViewModel
-import com.patrykandpatrick.vico.compose.axis.horizontal.rememberBottomAxis
-import com.patrykandpatrick.vico.compose.axis.vertical.rememberStartAxis
-import com.patrykandpatrick.vico.compose.chart.column.columnChart
-import com.patrykandpatrick.vico.core.entry.ChartEntryModelProducer
-import com.patrykandpatrick.vico.core.entry.entryOf
-import kotlin.random.Random
 
 @Composable
 fun ChartContent(
     contentPadding: PaddingValues,
     homeViewModel: HomeViewModel
 ) {
-    Chart(contentPadding)
+    Surface(
+        modifier = Modifier.padding(contentPadding).fillMaxSize()) {
+        Chart(
+            data = mapOf(
+                Pair(0.9f, "N"),
+                Pair(0.6f, "A"),
+                Pair(0.2f, "S"),
+                Pair(0.7f, "D"),
+            ),
+            max_value = 1000
+        )
+    }
 }
 
 @Composable
 fun Chart(
-    contentPadding: PaddingValues,
+    data: Map<Float, String>,
+    max_value: Int,
 ) {
-    val chartEntryModelProducer = ChartEntryModelProducer(getRandomEntries())
+    val context = LocalContext.current
+    // BarGraph Dimensions
+    val barGraphHeight by remember { mutableStateOf(100.dp) }
+    val barGraphWidth by remember { mutableStateOf(10.dp) }
+    // Scale Dimensions
+    val scaleYAxisWidth by remember { mutableStateOf(50.dp) }
+    val scaleLineWidth by remember { mutableStateOf(1.dp) }
 
     Column(
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
-            .fillMaxSize()
-            .padding(horizontal = 50.dp)
+            .padding(20.dp)
+            .width(220.dp).height(150.dp),
+        verticalArrangement = Arrangement.Top
     ) {
-        com.patrykandpatrick.vico.compose.chart.Chart(
-            chart = columnChart(),
-            chartModelProducer = chartEntryModelProducer,
-            startAxis = rememberStartAxis(),
-            bottomAxis = rememberBottomAxis(),
-        )
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(barGraphHeight),
+            verticalAlignment = Alignment.Bottom,
+            horizontalArrangement = Arrangement.Start
+        ) {
+            // graph
+            data.forEach {
+                Box(
+                    modifier = Modifier
+                        .padding(start = barGraphWidth, bottom = 5.dp)
+                        .clip(CircleShape)
+                        .width(barGraphWidth)
+                        .fillMaxHeight(it.key)
+                        .background(MaterialTheme.colorScheme.onPrimaryContainer)
+                        .clickable {
+                            Toast
+                                .makeText(context, it.key.toString(), Toast.LENGTH_SHORT)
+                                .show()
+                        }
+                )
+            }
+        }
     }
 }
-fun getRandomEntries() = List(4) { entryOf(it, Random.nextFloat() * 16f) }
-
 
